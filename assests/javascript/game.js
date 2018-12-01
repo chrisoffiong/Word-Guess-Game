4020614299dd9fad917fd347207a49963c6fb3f4
unknown <chrisoffiong@gmail.com>
2018-12-01 00:19:18 -0500

    revision

---
+++ assests/javascript/game.js
@@ -0,0 +1,6 @@
+var categories = ['Cars','Dogs','Cats','Famous Musicians','']
+var words = [ 'Telsa', 'Ford Focus', 'Miata', 'Bugatti Veyron','Shiba Inu','Husky','Chihuahua','German Shepard','Persian','Sphynx','Chartreux','Siamese',
+'Jimi Hendrix','Elvis Presley','Beyonce','Prince','q','r','s','t','u','v','w','x','y','z'];
+window.onload = function () {
+alert ("Hangman Revolution!");
+}
